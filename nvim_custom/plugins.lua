ed90c48e2ee9ccb77d5248e31bc03ee69b441eea
Aaron Lebahn <cplusplus328@gmail.com>
2023-10-01 21:07:56 -0500

    Trying to re-enable netrw

--- nvim_custom/plugins.lua
+++ nvim_custom/plugins.lua
@@ -16,6 +16,33 @@ local plugins = {
       vim.g.mkdp_open_to_the_world = 1
     end,
     ft = "markdown"
+  },
+  default_plugin_config_replace = {
+    nvim_tree = {
+      disable_netrw = false,
+      hijack_netrw = true,
+    }
+  },
+  builtins = {
+      "2html_plugin",
+      "getscript",
+      "getscriptPlugin",
+      "gzip",
+      "logipat",
+      --"netrw",
+      --"netrwPlugin",
+      --"netrwSettings",
+      --"netrwFileHandlers",
+      "matchit",
+      "tar",
+      "tarPlugin",
+      "rrhelper",
+      "spellfile_plugin",
+      "vimball",
+      "vimballPlugin",
+      "zip",
+      "zipPlugin",
+
   }
 }
 
