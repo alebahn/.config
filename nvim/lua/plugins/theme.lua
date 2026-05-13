8be2f2fadc1b172d9fa0f45f0849c8965d719f48
Aaron Lebahn <aaron.lebahn@i3pd.com>
2026-05-13 12:46:00 -0500

    Add portable theme.lua

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,1 +1,18 @@
-/home/alebahn/.config/omarchy/current/theme/neovim.lua
+local theme_file = vim.fn.stdpath("config") .. "/omarchy/current/theme/neovim.lua"
+
+if vim.fn.filereadable(theme_file) == 1 then
+	return dofile(theme_file)
+end
+
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000,
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "tokyonight-night",
+		},
+	},
+}
